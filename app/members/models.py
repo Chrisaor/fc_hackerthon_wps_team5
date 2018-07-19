0b93190afb301595f1afe6192c0fa8c0b53de154
parkjihye <zehye.01@gmail.com>
2018-07-19 23:29:36 +0900

    페이스북 로그인, 회원가입, 로그인, 로그아웃 views, template, forms, urls, backends.py

--- app/members/models.py
+++ app/members/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
     CHOICE_GENDER = (
         ('m', '남성'),
@@ -9,5 +10,6 @@ class User(AbstractUser):
     )
     introduce = models.TextField(blank=True)
     gender = models.CharField(max_length=1, choices=CHOICE_GENDER)
+
     def __str__(self):
         return f'{self.username}'
