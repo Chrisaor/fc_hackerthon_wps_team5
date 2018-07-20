53d390e0f0fc4f47f698db8e5390dd60b708f320
parkjihye <zehye.01@gmail.com>
2018-07-20 17:35:46 +0900

    template/search_flight.html 수정

--- app/flight_ticket/views.py
+++ app/flight_ticket/views.py
@@ -15,12 +15,12 @@ def flight_main(request):
     return render(request, 'flight_ticket/search_flight.html')
 
 def search_flight(request, origin, destination, month):
-    # instance = FlightInfo.objects.filter(destination=destination).filter(depart_month=month)
-    # if instance.exists():
-    #     instance.delete()
+    instance = FlightInfo.objects.filter(destination=destination).filter(depart_month=month)
+    if instance.exists():
+        instance.delete()
 
-    # flight_info = FlightInfo.objects.create(origin=origin, destination=destination, depart_month=month)
-    # flight_info.get_price_info()
+    flight_info = FlightInfo.objects.create(origin=origin, destination=destination, depart_month=month)
+    flight_info.get_price_info()
     flight = FlightInfo.objects.filter(destination=destination, depart_month=month).all()[0]
 
     prices = PriceInfo.objects.filter(flight__destination=destination, flight__depart_month=month)
